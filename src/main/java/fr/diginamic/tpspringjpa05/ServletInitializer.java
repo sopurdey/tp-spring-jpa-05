b84471e6a21102b1d8d55f058009b7c8b09b32d5
sopurdey <a_purdey@hotmail.com>
2022-01-06 09:54:42 +0100

    Initialisation projet Spring

---
+++ src/main/java/fr/diginamic/tpspringjpa05/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tpspringjpa05;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TpSpringJpa05Application.class);
+	}
+
+}
---
+++ src/main/java/fr/diginamic/tpspringjpa05/TpSpringJpa05Application.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tpspringjpa05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringJpa05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringJpa05Application.class, args);
+	}
+
+}
